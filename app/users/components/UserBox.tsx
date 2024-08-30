3e6015eeb407982b44f19fa8bf815c4e83dfdf05
saifmalik2004 <saifmalik7827@gmail.com>
2024-08-30 13:54:28 +0530

    conversation api added

--- app/users/components/UserBox.tsx
+++ app/users/components/UserBox.tsx
@@ -1,3 +1,4 @@
+import Avatar from '@/app/components/Avatar'
 import { User } from '@prisma/client'
 import axios from 'axios'
 import { useRouter } from 'next/navigation'
@@ -21,7 +22,18 @@ const UserBox:React.FC<UserBoxProps>=({
          .finally(()=> setIsloading(false));
     },[data,router])
   return (
-   <div onClick={handleClick} className="w-full">
+   <div onClick={handleClick} className="w-full relative flex items-center space-x-3 bg-white p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer">
+      <Avatar user={data}/>
+      <div className="min-w-0 flex-1 ">
+         <div className="focus:outline-none">
+            <div className="flex justify-between items-center mb-1">
+                  <p className="text-sm font-medium text-gray-900">
+                  {data.name}
+                  </p>
+            </div>
+         </div>
+
+      </div>
 
    </div>
   )
